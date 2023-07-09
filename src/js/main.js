b5c4a5bce1510fd3dddedf3f305d70eaa1517203
demacedius <demacedius@pop-os.localdomain>
2023-07-09 17:05:24 +0200

    last commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,40 +5,62 @@ myImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA0gAAAHWCAYAAABNFKn
 myImage.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    
     canvas.width = 840;
     canvas.height = 470;
     
+    ctx.drawImage(myImage, 0 , 0 , canvas.width, canvas.height);
     const pixel = ctx.getImageData(0,0, canvas.width, canvas.height);
+    ctx.clearRect(0,0,canvas.widht,canvas.height);
 
 
 
     let particlesArray = [];
-    const numberOfParticles = 5000;
+    const numberOfParticles = 7000;
 
     let mappedImage = [];
     for (let y = 0; y < canvas.height; y++){
         let row = [];
         for (let x = 0; x < canvas.width; x++){
             const red = pixel.data[(y * 4 * pixel.width) + (x * 4)];
-            const green = pixel.data[(y * 4 * pixel.width) + (x * 4)];
-            const blue = pixel.data[(y * 4 * pixel.width) + (x * 4)];
-            const alpha = pixel.data[(y * 4 * pixel.width) + (x * 4)];
+            const green = pixel.data[(y * 4 * pixel.width) + (x * 4 + 1)];
+            const blue = pixel.data[(y * 4 * pixel.width) + (x * 4 + 2)];
+            const brightness = calculateRelativeBrightness(red,green,blue);
+            const cell = [
+                cellBrightness = brightness,
+            ];
+            row.push(cell);
         }
+        mappedImage.push(row);
     }
+
+    function calculateRelativeBrightness(red,green,blue){
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green ) * 0.587+
+            (blue * blue) * 0.114
+        )/100;
+    }
+
     class Particle {
         constructor(){
             this.x = Math.random() * canvas.width;
             this.y = 0;
             this.speed = 0;
-            this.velocity = Math.random() * 3.5;
+            this.velocity = Math.random() * 1.5;
             this.size = Math.random() * 1.5 + 1;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
 
 
         }
 
         update(){
-            this.y+= this.velocity;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+            this.speed = mappedImage[this.position1][this.position2][0];
+            let movement = (2.5 - this.speed) + this.velocity;
+            this.y+= movement;
+
             if (this.y >= canvas.height){
                 this.y = 0;
                 this.x = Math.random() * canvas.width;
@@ -59,12 +81,13 @@ myImage.addEventListener('load', function(){
     init();
 
     function animate(){
-        ctx.drawImage(myImage, 0 , 0 , canvas.width, canvas.height);
         ctx.globalAlpha = 0.05;
         ctx.fillStyle = 'rgb(0,0,0)';
+        ctx.globalAlpha = 0.2
         ctx.fillRect(0,0,canvas.width,canvas.height);
         for(let i = 0; i < particlesArray.length; i++){
             particlesArray[i].update();
+            ctx.globalAlpha = particlesArray[i].speed * 0.8;
             particlesArray[i].draw();
         }
         requestAnimationFrame(animate);
